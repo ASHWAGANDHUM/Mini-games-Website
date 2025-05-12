2bb5b94042a9dee38500535d04f27fcf07ff7a30
ASHWAGANDHUM <agq772@gmail.com>
2025-05-12 21:03:59 +0800

    Task#3 has completed.

--- js-hw/hw-3.js
+++ js-hw/hw-3.js
@@ -9,3 +9,12 @@ for (let i = 0; i < 2; i++) {
 for (let i = 1; i <= 5; i++) {
     console.log(i);
 }
+
+// Задание 3
+
+for (let i = 0; i <= 22; i++) {
+    if (i < 7) {
+        continue;
+    }
+    console.log(i);
+}
