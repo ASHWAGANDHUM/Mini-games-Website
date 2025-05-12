bbb82b95eb1e3d9892533906d1f71d3fc039f0ca
ASHWAGANDHUM <agq772@gmail.com>
2025-05-12 21:10:04 +0800

    Task#4 has completed.

--- js-hw/hw-3.js
+++ js-hw/hw-3.js
@@ -18,3 +18,14 @@ for (let i = 0; i <= 22; i++) {
     }
     console.log(i);
 }
+
+// Задание 4
+
+const obj = {
+    Коля: 200,
+    Вася: 300,
+    Петя: 400
+}
+for (const key in obj) {
+    console.log(`${key} — з/п ${obj[key]} долларов`);
+}
