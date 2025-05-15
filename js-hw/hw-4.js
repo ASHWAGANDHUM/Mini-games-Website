c88c3779dc6debae39db6e5c5798c2e806a8eb26
ASHWAGANDHUM <agq772@gmail.com>
2025-05-15 18:49:13 +0800

    Task#2 has completed.

--- js-hw/hw-4.js
+++ js-hw/hw-4.js
@@ -11,3 +11,14 @@ let firstNumber = Number(prompt('Введите первое число'));
 let secondNumber = Number(prompt('Введите второе число'));
 alert(`Минимальное число: ${minOfTwo(firstNumber, secondNumber)}`);
 
+// Задание 2
+
+function isEven(number) {
+    return number % 2 === 0 ? `Число ${number} — чётное.`: `Число ${number} — нечётное.`;
+}
+let checkNumber = Number(prompt('Введите число.'));
+    if (isNaN(checkNumber)) {
+        alert('Это не число!');
+    } else {
+        alert(isEven(checkNumber));
+    }
