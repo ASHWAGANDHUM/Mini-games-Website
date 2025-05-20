d74f160dd00fe8d3f0f7a6417473f076d16a0c6a
ASHWAGANDHUM <agq772@gmail.com>
2025-05-20 20:26:06 +0800

    Task# 1 has completed.

---
+++ js-hw/hw-5.js
@@ -0,0 +1,11 @@
+// Задание 1
+
+const arr = [1, 5, 4, 10, 0, 3];
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 10) {
+        console.log(arr[i]);
+        break
+    }
+    console.log(arr[i]);
+}
