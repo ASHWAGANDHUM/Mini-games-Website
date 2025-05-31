5d88578a26da22b6504fa30d5855a5844896d243
ASHWAGANDHUM <agq772@gmail.com>
2025-06-01 04:20:33 +0800

    Task#2 has completed.

--- js-hw/hw-6.js
+++ js-hw/hw-6.js
@@ -3,3 +3,16 @@
 
 const str = 'js';
 console.log(str.toUpperCase());
+
+// Задание 2.
+// Создать функцию, которая принимает массив строк и строку. Функция должна вернуть новый массив, содержащий только те элементы первого массива, которые начинаются со второй строки. Регистр символов не влияет на результат.
+
+const beverages = ['Чай чёрный', 'Лимонад со вкусом манго', 'Кофе зерновой',
+'Лимонад Колокольчик', 'Чай зелёный', 'Сок апельсиновый'];
+
+function filterArrayByString(arr, str) {
+    return arr.filter(item => item.toLowerCase().startsWith(str.toLowerCase()));
+}
+
+const filteredBeverages = filterArrayByString(beverages, 'Лимонад');
+console.log(filteredBeverages);
