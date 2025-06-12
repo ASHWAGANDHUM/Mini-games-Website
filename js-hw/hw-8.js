58d40c5cf12a322309f793069f6f79ba85496d67
ASHWAGANDHUM <agq772@gmail.com>
2025-06-12 20:53:23 +0800

    Task#2 has completed.

--- js-hw/hw-8.js
+++ js-hw/hw-8.js
@@ -1,12 +1,28 @@
 // Задание 1. Скрытие и показ текста
 // Задача: cоздайте кнопку, которая будет скрывать и показывать текст в заголовке <h1>.
 
-const textEl = document.querySelector('.text');
-const btnEl = document.querySelector('.button');
+const headingEl = document.querySelector('.heading');
+const btnEl = document.querySelector('.btn-hidden');
 
-console.log(textEl);
+console.log(headingEl);
 console.log(btnEl);
 
 btnEl.addEventListener('click', () => {
-        textEl.classList.toggle('text_hidden');
+    headingEl.classList.toggle('text_hidden');
+});
+
+// Задание 2. Изменение стиля элемента
+// Задача: создайте кнопку, которая изменяет цвет текста в элементе <p>.
+
+// Инструкция:
+
+// В HTML создайте абзац <p> и кнопку. Используйте метод querySelector для получения ссылки на абзац и кнопку. Используйте событие click и метод addEventListener, чтобы изменить стиль элемента, например его цвет.
+
+// Как должно работать: нажатие на кнопку «Изменить цвет» делает текст абзаца синим.
+
+const textEl = document.querySelector('.text');
+const btnColor = document.querySelector('.btn-change-color');
+
+btnColor.addEventListener('click', () => {
+    textEl.style.color = 'blue';
 });
